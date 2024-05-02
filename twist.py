a8ba608f407a9e36c1893b9a9be9ca1ec0eb0e06
ikchangcho <142732209+ikchangcho@users.noreply.github.com>
2024-05-02 17:02:55 -0500

    Commit from Macbook Air

--- twist.py
+++ twist.py
@@ -3,20 +3,29 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
-data = np.genfromtxt('twist_over_time.csv', delimiter=",")
+for i in range(1, 61):
+    data = np.zeros(99)
+    for j in range(1, 5):
+        data += np.genfromtxt(f'/Users/ik/Pycharm/Mitchell/250502 Twist, Ribbon {j}/twist_ribbon{j}_time{i:02}.csv', delimiter=',')
+    data = data / 4
+    np.savetxt(f'average_twist_time{i}', data, delimiter=",")
 
-fig, ax = plt.subplots()
-labels = ['Ribbon 1', 'Ribbon 2', 'Ribbon 3', 'Ribbon 4']
 
-for i in range(4):
-    ax.plot(data[i], label=labels[i])
-ax.legend(fontsize=10)
-ax.grid()
-ax.set_xlabel("Time")
-ax.set_ylabel("Twist (rad)")
-ax.set_title("Twist at the end of the curve")
-plt.savefig("Twist vs Time.png")
-plt.show()
+
+# data = np.genfromtxt('twist_over_time.csv', delimiter=",")
+#
+# fig, ax = plt.subplots()
+# labels = ['Ribbon 1', 'Ribbon 2', 'Ribbon 3', 'Ribbon 4']
+#
+# for i in range(4):
+#     ax.plot(data[i], label=labels[i])
+# ax.legend(fontsize=10)
+# ax.grid()
+# ax.set_xlabel("Time")
+# ax.set_ylabel("Twist (rad)")
+# ax.set_title("Twist at the end of the curve")
+# plt.savefig("Twist vs Time.png")
+# plt.show()
 
 
 # Twist over time
