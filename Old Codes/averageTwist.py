ea85b6f156b78cfe07998f7cd8ab31dc12a1a8b9
ikchangcho <142732209+ikchangcho@users.noreply.github.com>
2024-05-08 16:38:24 -0500

    Commit from Macbook Pro

---
+++ Old Codes/averageTwist.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+data1 = np.genfromtxt("../twist_rate_ribbon1.csv", delimiter=",", skip_header=1)
+data2 = np.genfromtxt("../twist_rate_ribbon2.csv", delimiter=",", skip_header=1)
+data3 = np.genfromtxt("../twist_rate_ribbon3.csv", delimiter=",", skip_header=1)
+data4 = np.genfromtxt("../twist_rate_ribbon4.csv", delimiter=",", skip_header=1)
+data = np.genfromtxt("../torsion.csv", delimiter=",", skip_header=1)
+np.savetxt("../twist_rate_average.csv", data, delimiter=",", header="Row: Time, Column: Sampled points")
+
+
+
+for i in range(1, 61):
+    data = np.zeros(99)
+    for j in range(1, 5):
+        data += np.genfromtxt(f'/Users/ik/Pycharm/Mitchell/250502 Twist, Ribbon {j}/twist_ribbon{j}_time{i:02}.csv', delimiter=',')
+    data = data / 4
+    np.savetxt(f'average_twist_time{i}', data, delimiter=",")
+
+
+
--- Old Codes/data.py
+++ Old Codes/data.py
@@ -5,4 +5,4 @@ for j in range(1, 2):
     for i in range(0, 60):
         data[i] = np.genfromtxt(f"/Users/ik/Pycharm/Mitchell/240502 Average Twist/average_twist_time{i+1}")
 
-    np.savetxt(f"twist_average.csv", data, delimiter=",", header="Row: Time, Column: Sampled points")
+    np.savetxt(f"../twist_average.csv", data, delimiter=",", header="Row: Time, Column: Sampled points")
